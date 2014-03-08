7642bace96c7ac8657df08a997e20df6b6e161de
Jeromy <jeromyj@gmail.com>
2014-03-08 22:44:02 +0000

    unclean some code and add info about selecting a hash function

--- coin.go
+++ coin.go
@@ -1,31 +1,19 @@
 package main
-
-import (
-	"fmt"
+import ("fmt"
 	"crypto/sha256"
-	"encoding/hex"
-)
-
+	"encoding/hex")
 func FindHash(diff uint, data string) (int, string) {
 	for i := 0; true; i++ {
 		out := sha256.Sum256([]byte(fmt.Sprintf("%s%d", data,i)))
-		if checkBits(diff, out[:]) {
-			return i,hex.EncodeToString(out[:])
-		}
+		if checkBits(diff, out[:]) {return i,hex.EncodeToString(out[:])}
 	}
 	return 0,""
 }
-
-func main() {
-	fmt.Println(FindHash(20, "Hello"))
-}
-
+func main() { fmt.Println(FindHash(5, "Hello")) }
 func checkBits(dif uint, b []byte) bool {
 	for _,v := range b {
 		if dif >= 8 {
-			if v != 0 {
-				return false
-			}
+			if v != 0 { return false }
 			dif -= 8
 		} else if dif > 0 {
 			return (v & ^(0xff >> dif)) == 0
